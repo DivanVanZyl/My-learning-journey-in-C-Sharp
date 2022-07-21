d2a25b9e2a0e810c3e8257a7a2b4bc954d3a41ab
Divan van Zyl <divanvanzyl1234@gmail.com>
2022-07-21 22:32:35 +0200

    Floating point operations

---
+++ Advanced CSharp - Floating-Point Types/Program.cs
@@ -0,0 +1,44 @@
+﻿//FP calcs do not cause exceptions:
+//Div of 0 is allowed!
+//FP types define the idea of infinity:
+var di = double.PositiveInfinity;
+var fi = float.PositiveInfinity;
+//only thing you can do with infinity is flip the sign. Once you have it, you are stuck with that state
+//eg -1.0 * fi
+
+//NaN is: Not a Number, any operation on it yields a NaN eg. double.Nan == (1.0/0.0) //false
+
+//Presentation problems:
+Console.WriteLine("FP numbers are stored in binary, for eg. 0.1 is stored as repeating bin numbers: 0.000110011001100110011....");
+Console.Write("This can lead to surprising results. \nFor eg. 0.1 + 0.2 = ");
+double d = 0.1 + 0.2;
+Console.Write(d + "\n\n");
+
+Console.WriteLine($"Comparing using direct floating point arithmatic (0.1 + 0.2) == 0.3");
+if ((0.1 + 0.2) == 0.3)
+{
+    Console.WriteLine("Success!");
+}
+else
+{
+    Console.WriteLine("Failure");
+}
+
+Console.Write(Environment.NewLine);
+
+//Comparisons need to use tolerance value:
+//if(Math.Abs(x - y) < 1e-8)
+//You will have to find the tolerance values.
+double toleranceValue = 1e-8;
+Console.WriteLine($"Comparing using tolerance value {toleranceValue}");
+if (Math.Abs(d - 0.3) < toleranceValue)
+{
+    Console.WriteLine("Success!");
+}
+else
+{
+    Console.WriteLine("Failure, incorrect tolerance value");
+}
+
+
+Console.ReadKey();
--- Advanced CSharp - Numerics/Program.cs
+++ Advanced CSharp - Numerics/Program.cs
@@ -16,7 +16,7 @@ if (Avx.IsSupported)
     var y = Vector128.Create(1.0f);
     var f = Sse.Add(x, y);  //NOTE: No operator support, intrintic calls map onto CPU intsuctions!
 
-    //Adding 4 pairs of floats
+    //Adding 4 pairs of floats. Single Instruction Multiple Data calculation
     var v1 = Vector128.Create(1.0f,2.0f,3.0f,4.0f); //Allocate 128 bit registers full of floating point numbers. IOW packs as many floats of that value in the register as possible
     var v2 = Vector128.Create(1.0f, 2.0f, 4.0f, 8.0f);
     var sum = Sse.Add(v1, v2);
