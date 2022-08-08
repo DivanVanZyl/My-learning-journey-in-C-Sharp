f275b5415df5e67d6e61830fc22fa8f468664d03
Divan van Zyl <divanvanzyl1234@gmail.com>
2022-08-08 16:22:45 +0200

    IDisposable

---
+++ IDisposable/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace disp
+{
+    public class MyClass : IDisposable
+    {
+        public MyClass()
+        {
+            Console.WriteLine("Hello");
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("Goodbye");
+        }
+    }
+    public class Disposable : IDisposable
+    {
+        private readonly Action end;
+        private Disposable (Action start, Action end)
+        {
+            this.end = end;
+            start();
+        }
+        public void Dispose()
+        {
+            end();
+        }
+        public static Disposable Create(Action start, Action end)
+        {
+            return new Disposable (start, end);
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            //Simple utilisation of IDisposable
+            using (var mc = new MyClass())
+            {
+
+            }
+            
+            //Wrapped in Disposable class
+            var st = new Stopwatch();
+            using (Disposable.Create(
+                () => st.Start(),
+                () => st.Stop()
+                ))
+            {
+                Console.WriteLine("...something here");
+                Thread.Sleep(1000);
+                Console.WriteLine(st.ElapsedMilliseconds);
+            }
+        }
+    }
+}
+
