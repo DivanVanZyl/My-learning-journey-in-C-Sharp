a770a90929ce5f2be5bce1ecc18c7836b1b308d5
Divan van Zyl <divanvanzyl1234@gmail.com>
2022-08-08 16:07:00 +0200

    Span of T

---
+++ Span of T/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+unsafe
+{
+    //Managed memory
+    byte* ptr = stackalloc byte[4];
+    Span<byte> memory = new Span<byte> (ptr, 4);
+
+    //Unmanaged
+    IntPtr unmanagedPtr = Marshal.AllocHGlobal (123);
+    Span<byte> unmanagedMemory = new Span<byte>(unmanagedPtr.ToPointer (), 123);
+    Marshal.FreeHGlobal (unmanagedPtr);
+}
+
+char[] stuff = "hello".ToCharArray();
+Span<char> arraymemory = stuff; //automatically init a Span which refers to the acual letters inside "hello"
+
+ReadOnlySpan<char> more = "hi there!".AsSpan();
+
+Console.WriteLine($"Out span has {more.Length} elements");
+
+//Some operations on Span
+arraymemory.Fill('x');  //MODIFY array
+Console.WriteLine(stuff);
+arraymemory.Clear();
+Console.WriteLine(stuff);
