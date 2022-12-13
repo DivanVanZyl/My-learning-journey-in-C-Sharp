3bc7d4726de94e2907593c5e1c37ac3a3becf91c
Divan van Zyl <divanvanzyl1234@gmail.com>
2022-12-13 22:08:06 +0200

    CompositeCancellationTokens

---
+++ CompositeCancellationTokens/Program.cs
@@ -0,0 +1,24 @@
+﻿var planned = new CancellationTokenSource();
+var preventative = new CancellationTokenSource();
+var emergency = new CancellationTokenSource();
+
+//We can can specify a token that reacts to any of those 3 tokens being triggered.
+//This is a linked token source. In this example, if any one of planned, preventative or emergency tokens are called, paranoid will be called.
+var paranoid = CancellationTokenSource.CreateLinkedTokenSource(
+    planned.Token, preventative.Token, emergency.Token);
+
+Task.Factory.StartNew(() =>
+{
+    int i = 0;
+    while(true)
+    {
+        paranoid.Token.ThrowIfCancellationRequested();
+        Console.WriteLine($"{i++}\t");
+        Thread.Sleep(1000);
+    }
+}, paranoid.Token);
+
+Console.ReadKey();
+emergency.Cancel();
+
+Console.WriteLine("Main program done.");
