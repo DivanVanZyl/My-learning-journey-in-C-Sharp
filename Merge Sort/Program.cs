b6d5e0157716617d7b0c88fe41e7f935ab49ce81
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-01-05 21:31:16 +0200

    Quick Sort

--- Merge Sort/Program.cs
+++ Merge Sort/Program.cs
@@ -1,14 +1,7 @@
-﻿// "Devide and conquer" idea.
-// Two phases: splitting and merging
-// Splitting is logical: Provides an organized way to sequence the merges.
-// Note: NOT and in-place algorithm.
-// Uses significant amount of memory. Amount depends on n.
-// Stable: relative position mantained (in a classic implementation)
-// O (nlogn) time complexity (linearithmic)
-
-using Merge_Sort;
+﻿using Sort;
 
 int[] arr = new int[] { 1, 4, 2 };
-Sorting.MergeSort(arr);
+//Sorting.MergeSort(arr);
+Sorting.QuickSort(arr);
 foreach(int i in arr)
     Console.Write(i);
--- Merge Sort/Sorting.cs
+++ Merge Sort/Sorting.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Merge_Sort
+namespace Sort
 {
     public class Sorting
     {
+        // "Devide and conquer" idea.
+        // Recursive
+        // Two phases: splitting and merging
+        // Splitting is logical: Provides an organized way to sequence the merges.
+        // Note: NOT and in-place algorithm.
+        // Uses significant amount of memory. Amount depends on n.
+        // Stable: relative position mantained (in a classic implementation)
+        // O (nlogn) time complexity (linearithmic)
         public static void MergeSort(int[] array)
         {
             int[] aux = new int[array.Length];
@@ -42,6 +51,60 @@ namespace Merge_Sort
                         array[k] = aux[i++];
                 }
             }
+        }
+        // "Devide and conquer" idea.
+        // Recursive
+        // In-place algorithm, don't have to use extra memory
+        // typically O (nlogn) time complexity (linearithmic). But, O(n^2) (quadtratic) in extreamly rare, worst case scenario (inverse case).
+        // Unstable
+        public static void QuickSort(int[] array)
+        {
+            Sort(0, array.Length - 1);
+            
+            void Sort(int low, int high)
+            {
+                if (high <= low)
+                    return;
+                int j = Partition(low, high);
+                Sort(low, j-1);
+                Sort(j + 1, high);
+            }
+
+            int Partition(int low, int high)
+            {
+                int i = low;
+                int j = high + 1;
+
+                int pivot = array[low];
+                while(true)
+                {
+                    while (array[++i] < pivot)
+                    {
+                        if (i == high)
+                            break;
+                    }
+
+                    while(pivot < array[--j])
+                    {
+                        if(j == low)
+                            break;
+                    }
+
+                    if(i >= j)
+                        break;
+
+                    Swap(array, i, j);
+                }
+                Swap(array, low, j);
+                return j;
+            }
+        }
+        private static void Swap(int[] array, int i, int j)
+        {
+            int tmp;
+            tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
         }
     }
 }
