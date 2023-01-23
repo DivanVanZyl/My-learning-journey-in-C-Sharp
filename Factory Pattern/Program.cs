1a3a0908aadff59ef9b8e7b3e7a89328b62cef7d
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-01-23 22:24:48 +0200

    Factory pattern comments

---
+++ Factory Pattern/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+ * There are some limitations to constuctors, therefore the factory pattern may be usefull.
+ * For example, we cannot destinguish between constructors, based on variable names passed. So then you generally have to introduce some other type to distinguish
+ * In the above, there is more paramaters in the constructor than is ideal.
+ * 
+ * 2 types of factories: Proper and abstract.
+ * 
+ * A factory is essentially a method that returns instances of an object, using a private constructor.
+ * 
+ */
+Console.WriteLine("Hello, World!");
