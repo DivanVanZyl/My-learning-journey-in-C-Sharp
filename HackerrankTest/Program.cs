6bc31a47f215f4da13981302af77cc5159b2827f
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-01-25 15:27:59 +0200

    Added project for hackerrank debugging

---
+++ HackerrankTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using AlgTester.API;//Include lib
+using HackerrankTest;
+
+// Save the function you want to test in a variable (it will help C# auto resolve the correct method call)
+var solutionFunc = Solution.MyCodingChallengeSolution;
+// Run your tests
+SolutionTester.New()
+    .WithSolution(solutionFunc)
+    .WithTestCase(2, new int[] { 1, 3 }, 0) // Type safe Input and output
+    .WithTestCase(3, new int[] { 2, 3, 5 }, 1) // Another test case (this one will fail)
+    .WithTestCase(3, new int[] { 2, 3, 5 }, 0) // and another :)
+    .Run();//Run tests!
---
+++ HackerrankTest/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerrankTest
+{
+    internal static class Solution
+    {
+        public static int MyCodingChallengeSolution(int n, int[] arr)
+        {
+            return 0;
+        }
+    }
+}
