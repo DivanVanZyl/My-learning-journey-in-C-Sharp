cb29c58caf3eb6b2d1899e1ac5203cb7181359ea
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-07-05 20:33:02 +0200

    Enumerable class generation methods

---
+++ Generation Operations/Program.cs
@@ -0,0 +1,15 @@
+﻿var strings = Enumerable.Repeat("Hello", 3);
+foreach (var s in strings)
+    Console.WriteLine(s);
+
+var ints  = Enumerable.Range(1, 10);
+foreach (var i in ints)
+    Console.WriteLine(i);
+
+var chars = Enumerable.Range('a', 'z' - 'a').Select(c => (char)c);
+foreach (var c in chars)
+    Console.WriteLine(c);
+
+var myString = Enumerable.Range(1,10).Select(i => new string('x',i));
+foreach (var s in myString)
+    Console.WriteLine(s);
