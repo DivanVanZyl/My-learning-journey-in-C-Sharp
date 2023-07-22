a0942d24937df918b1b70a9520d13deb9747df0b
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-07-22 22:58:44 +0200

    Linq: Filtering data and types

---
+++ Filtering Data/Program.cs
@@ -0,0 +1,17 @@
+﻿var numbers = Enumerable.Range(1, 10);
+var evenNumbers  = numbers.Where(n => n%2 == 0);
+foreach(var number in evenNumbers)
+    Console.WriteLine(number);
+
+//Can combine projection (Select) and Filtering (Where)
+Console.WriteLine("Combination");
+var oddNumbers = numbers.Select(x => x*x).Where(y => y%2 == 1);
+foreach(var number in oddNumbers)
+    Console.WriteLine(number);
+
+//We can even filter different data types!
+Console.WriteLine("Different data types");
+object[] values = { 1, 1.5, 3, 4.5};    //This contains both ints and doubles
+Console.WriteLine("These are the ints: ");
+foreach (var obj in values.OfType<int>())   //I create a new collection here, with only the specified types!
+    Console.WriteLine(obj);
