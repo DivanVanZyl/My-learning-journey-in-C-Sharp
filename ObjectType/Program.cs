e91a6ce5c5ce25c2eb27662e7b21924661598072
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-07-28 19:01:05 +0200

    Object type

---
+++ ObjectType/Program.cs
@@ -0,0 +1,22 @@
+﻿int myInt = 32;
+int myOtherInt = 2;
+string myString = "This is a string";
+
+List<object> list = new List<object>
+{
+    32,
+    2,
+    "This is a string."
+};
+
+int product = 1;
+foreach(object obj in list)
+{
+    if(obj is string)
+        Console.WriteLine($"String value: {obj}");
+
+    if(obj is int)
+        product *= (int)obj;
+}
+
+Console.WriteLine($"Sum is: {product}");
