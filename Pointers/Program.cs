519ac31c2e084548750d66ec519e8967145764fb
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-08-13 14:42:28 +0200

    Pointers

---
+++ Pointers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Pointers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                int i = 5;
+                int *myPtr = & i;
+
+                Console.WriteLine($"This is my memory address: {(int)myPtr}");
+                Console.WriteLine($"The value in my memory address is: {*myPtr}");
+
+                Console.WriteLine($"Lets see what is at another memory address...");
+                for(int cnt = 0;cnt < 5;cnt++)
+                {
+                    myPtr += 1;
+                    Console.WriteLine($"This is my new memory address: {(int)myPtr}");
+                    Console.WriteLine($"The value in my new memory address is: {*myPtr}");
+                }
+            }
+        }
+    }
+}
