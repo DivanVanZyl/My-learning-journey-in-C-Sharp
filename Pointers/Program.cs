c032fef77eadb4a35d0bf0252417d1371ee6e88c
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-08-13 14:49:20 +0200

    Comment on pointer addition.

--- Pointers/Program.cs
+++ Pointers/Program.cs
@@ -15,7 +15,11 @@
                 Console.WriteLine($"Lets see what is at another memory address...");
                 for(int cnt = 0;cnt < 5;cnt++)
                 {
-                    myPtr += 1;
+                    /*
+                     * In C# an int is 4 bytes. This is why, with the pointer incrementation (myPtr++;), the pointers address is incremented by 4:
+                     * Pointer addition is a logic and not a numeric operation. Because, otherwise incrementing it by 1 would produce a pointer that is pointing to the middle of an object, which is invalid.
+                     */
+                    myPtr++;
                     Console.WriteLine($"This is my new memory address: {(int)myPtr}");
                     Console.WriteLine($"The value in my new memory address is: {*myPtr}");
                 }
