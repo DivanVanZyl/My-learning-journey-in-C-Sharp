2e6b81e4f3a0ebabf524c963bdf1dc3665074760
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-11-21 21:54:36 +0200

    Added projects

---
+++ HtmlBuilder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HtmlBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
