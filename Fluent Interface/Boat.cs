fb93b434edca120d9ceb8cd4b8879d0e2e9d10a6
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-12-07 20:22:00 +0200

    Fluent interface

---
+++ Fluent Interface/Boat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluent_Interface
+{
+    internal class Boat
+    {
+        private string _name;
+        private bool _hasSails;
+
+        public string Name => _name;
+        public bool HasSails => _hasSails;
+
+        private Boat() { }
+        public static Boat BuildBoat()
+        {
+            return new Boat();
+        }
+
+        public Boat WithSails()
+        {
+            _hasSails = true;
+            return this;
+        }
+
+        public Boat WithName(string name)
+        {
+            _name = name;
+            return this;
+        }
+    }
+}
---
+++ Fluent Interface/Program.cs
@@ -0,0 +1,18 @@
+﻿using Fluent_Interface;
+
+var boat = Boat.BuildBoat()
+    .WithSails()
+    .WithName("Blu boi");
+
+Console.WriteLine("I have a boat");
+
+if(boat.HasSails)
+{
+    Console.WriteLine("It has sails.");
+}
+else
+{
+    Console.WriteLine("It does not have sails.");
+}
+
+Console.WriteLine($"And it's name is: {boat.Name}");
