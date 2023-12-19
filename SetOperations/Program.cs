a5de9eb8ac96e936b505f7d425db905d4d135bb6
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-12-19 20:06:46 +0200

    Set operations

--- SetOperations/Program.cs
+++ SetOperations/Program.cs
@@ -5,4 +5,41 @@ using Shared;
 string word1 = "helloooo";
 string word2 = "help😲";
 
-word1.Distinct().ForEach;
+var distinctLetters = word1.Distinct();
+
+Console.WriteLine();
+foreach (var  word in distinctLetters)
+{
+    Console.WriteLine(word);
+}
+
+//Intersections
+//Distinct letters in both words
+var lettersInBoth = word1.Intersect(word2);
+
+Console.WriteLine();
+foreach (var word in distinctLetters)
+{
+    Console.WriteLine(word);
+}
+
+//Union
+//Get unique letters to create unique collection
+var uniqueletters = word1.Union(word2);
+
+Console.WriteLine();
+foreach (var word in uniqueletters)
+{
+    Console.WriteLine(word);
+}
+
+//Except
+//All elements in one collection that is not in another collection
+
+var onlyInWordOne = word1.Except(word2);
+
+Console.WriteLine();
+foreach (var word in onlyInWordOne)
+{
+    Console.WriteLine(word);
+}
