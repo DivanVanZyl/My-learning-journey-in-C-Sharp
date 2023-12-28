8adc60297392a2160fea16f7c30b31ea1823cc93
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-12-28 20:09:11 +0200

    Linq - Quantifier Operations

---
+++ Quantifier Operations/Program.cs
@@ -0,0 +1,13 @@
+﻿//These determine if one or more elements in a collection satisfy the predicate
+int[] numbers = { 1, 2, 3, 4, 5 };
+
+Console.WriteLine("Are all numbers > 0? " +
+    numbers.All(n => n >0));
+
+Console.WriteLine("Is any number < 2? " +
+    numbers.Any(n => n < 2));
+
+Console.WriteLine(new int[] { }.Any()); //Can check if a collection is empty like this
+
+Console.WriteLine("Contains 5? " +
+    numbers.Contains(5));
