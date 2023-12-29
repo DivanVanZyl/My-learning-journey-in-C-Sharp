c7a8455173f8285f94b365a5406fdb40f99e6ac6
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-12-29 15:46:26 +0200

    Linq - Concatenation Operations

---
+++ Concatenation Operations/Program.cs
@@ -0,0 +1,7 @@
+﻿using Shared;
+var intTypes  = new[] { typeof(int), typeof(short) };
+var fpTypes  = new[] { typeof(float), typeof(double) };
+
+var collection = intTypes.Concat(fpTypes).Prepend(typeof(bool));
+
+collection.ForEach(x => Console.WriteLine(x));
--- Shared/Extensions.cs
+++ Shared/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Shared
 {
     public static class Extensions
     {
+        //Short syntax to write out a collection
         public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
         {
             foreach (T item in enumerable)
@@ -15,5 +17,13 @@ namespace Shared
                 action(item);
             }
         }
+        /*public static IEnumerable<T> Prepend<T>(
+            this IEnumerable<T> values, T value)
+        {
+            yield return value;
+            foreach (var item in values)
+                yield return item;
+        }*/
     }
+
 }
