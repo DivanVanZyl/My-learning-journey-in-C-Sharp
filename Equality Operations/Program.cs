23a4cb708727fdd6676d674a042946f2ea298e33
Divan van Zyl <divanvanzyl1234@gmail.com>
2023-12-29 15:25:33 +0200

    Linq - Equality Operations

---
+++ Equality Operations/Program.cs
@@ -0,0 +1,11 @@
+﻿var arr1 = new[] { 1,2,3 };
+var arr2 = new[] { 1,2,3 };
+
+Console.WriteLine(arr1 == arr2);    //FALSE!
+//How to compare by value instead of reference
+
+Console.WriteLine(arr1.SequenceEqual(arr2));
+
+var list1 = new List<int> { 1,2,3 };
+
+Console.WriteLine(list1.SequenceEqual(arr2));   //Also true.
