46e5d50c5c25266872eddf9b64f5eeea1c42447e
Divan van Zyl <divanvanzyl1234@gmail.com>
2024-02-24 15:24:39 +0200

    DI

---
+++ CustomStringBuilder/Program.cs
@@ -0,0 +1,17 @@
+﻿/*NOTE: StringBuilder is SEALED. Therefore, we cannot inherit from it.*/
+using System.Text;
+
+namespace CustomStringBuilder
+{
+    internal class Program
+    {
+        public class CodeBuilder
+        {
+            private StringBuilder builder = new StringBuilder();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- DIConsoleApp/Services/MyOtherService.cs
+++ DIConsoleApp/Services/MyOtherService.cs
@@ -8,25 +8,25 @@ using System.Threading.Tasks;
 
 namespace DIConsoleApp.Services
 {
-    public class MyService : IMyService
+    public class MyOtherService : IMyService
     {
         private readonly string _baseUrl;
         private readonly string _token;
-        private readonly ILogger<MyService> _logger;
+        private readonly ILogger<MyOtherService> _logger;
 
-        public MyService(ILoggerFactory loggerFactory, IConfigurationRoot config)
+        public MyOtherService(ILoggerFactory loggerFactory, IConfigurationRoot config)
         {
             var baseUrl = config["SomeConfigItem:BaseUrl"];
             var token = config["SomeConfigItem:Token"];
 
             _baseUrl = baseUrl;
             _token = token;
-            _logger = loggerFactory.CreateLogger<MyService>();
+            _logger = loggerFactory.CreateLogger<MyOtherService>();
         }
 
         public async Task MyServiceMethod()
         {
-            Console.WriteLine("My other service method has run!");
+            Console.WriteLine("My service method has run!");
             _logger.LogDebug(_baseUrl);
             _logger.LogDebug(_token);
         }
