2fdb84be385da0809d2d9ca05fdf53a3e1d0659b
Divan van Zyl <divanvanzyl1234@gmail.com>
2024-03-02 14:44:34 +0200

    Dynamic programming - Memoiazation - HowSum

--- HowSum Memoization/Program.cs
+++ HowSum Memoization/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Shared;
+using static HowSumMethods;
+
+HowSumMemo(7, [2, 3])!.WriteCollection();
+HowSumMemo(7, [5, 3, 4, 7])!.WriteCollection();
+HowSumMemo(7, [2, 4])!.WriteCollection();
+HowSumMemo(8, [2, 3, 5])!.WriteCollection();
+HowSumMemo(300, [7, 14])!.WriteCollection();
+
+public static class HowSumMethods
+{
+    //time: O(n^m * m)
+    //space: O(m)
+    public static List<int>? HowSum(int targetSum, List<int> numbers)
+    {
+        if (targetSum == 0) return [];
+        if (targetSum < 0) return null;
+
+        foreach (int num in numbers)
+        {
+            int remainder = targetSum - num;
+            var remainderResult = HowSum(remainder, numbers);
+            if (remainderResult is not null)
+            {
+                remainderResult.Add(num);
+                return remainderResult;
+            }
+        }
+        return null;
+    }
+
+    //time: O(n*m^2)
+    //space: O(m^2)
+    public static List<int>? HowSumMemo(int targetSum, List<int> numbers, Dictionary<int,List<int>>? memo = null)
+    {
+        if(memo is null) memo = new Dictionary<int,List<int>>();
+        if (memo.ContainsKey(targetSum)) return memo[targetSum];
+        if (targetSum == 0) return [];
+        if (targetSum < 0) return null;
+
+        foreach (int num in numbers)
+        {
+            int remainder = targetSum - num;
+            var remainderResult = HowSumMemo(remainder, numbers,memo);
+            if (remainderResult is not null)
+            {
+                remainderResult.Add(num);
+                memo[targetSum] = remainderResult;
+                return memo[targetSum];
+            }
+        }
+        memo[targetSum] = null;
+        return memo[targetSum];
+    }
+}
--- Shared/Extensions.cs
+++ Shared/Extensions.cs
@@ -17,6 +17,46 @@ namespace Shared
                 action(item);
             }
         }
+
+        public static void WriteCollection<T>(this IEnumerable<T> enumerable,
+            ConsoleColor bracketColor = ConsoleColor.White,
+            ConsoleColor elementColor = ConsoleColor.Cyan,
+            ConsoleColor warningColor = ConsoleColor.DarkYellow,
+            ConsoleColor seperatorColor = ConsoleColor.DarkGray)
+        {
+            if (enumerable is null)
+            {
+                Console.Write('[');
+                Console.ForegroundColor = warningColor;
+                Console.Write("null");
+                Console.ForegroundColor = bracketColor;
+                Console.Write(']');
+                Console.WriteLine();
+            }
+            else if (!enumerable.Any())
+            {
+                Console.WriteLine("[]");
+            }
+            else
+            {
+                //Console.BackgroundColor = ConsoleColor.DarkGreen;
+                //Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write('[');
+                for (int i = 0; i < enumerable.Count(); i++)
+                {
+                    Console.ForegroundColor = elementColor;
+                    Console.Write(enumerable.ElementAt(i));
+                    if (!(i == enumerable.Count() - 1))
+                    {
+                        Console.ForegroundColor = seperatorColor;
+                        Console.Write(',');
+                    }
+                }
+                Console.ForegroundColor = bracketColor;
+                Console.Write(']');
+                Console.WriteLine();
+            }
+        }
         /*public static IEnumerable<T> Prepend<T>(
             this IEnumerable<T> values, T value)
         {
