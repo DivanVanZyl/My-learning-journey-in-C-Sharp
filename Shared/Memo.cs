18b347a888c1fafb78100733cc3a4e334438fbfe
Divan van Zyl <divanvanzyl1234@gmail.com>
2024-03-02 13:21:38 +0200

    Memo class

---
+++ Shared/Memo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class Memo<TKey, TValue> where TKey : notnull
+    {
+        private Dictionary<TKey, TValue> _memo = new();
+
+        public void Add(TKey key, TValue value)
+        {
+            _memo[key] = value;
+        }
+
+        public TValue GetValue(TKey key)
+        {
+            return _memo[key];
+        }
+    }
+}
