ae07c27723674c469eed4f2fd0b0db7d103180f6
Divan van Zyl <divanvanzyl1234@gmail.com>
2024-03-17 13:41:38 +0200

    Fixed bug

--- BestSum Memoization/Program.cs
+++ BestSum Memoization/Program.cs
@@ -2,9 +2,18 @@
 using static BestSumMethods;
 
 BestSum(7, [5, 3, 4, 7])!.WriteCollection();
+BestSum(8, [2, 3, 5])!.WriteCollection();
+BestSum(8, [1, 4, 5])!.WriteCollection();
+BestSum(100, [1, 2, 5, 25])!.WriteCollection();
 
 static class BestSumMethods
 {
+    //m = target sum
+    //n = numbers.length
+
+    //Brute force tree 
+    //time O(n^m * m)
+    //space O(m^2)
     internal static List<int>? BestSum(int targetSum, List<int> numbers)
     {
         if (targetSum == 0) return [];
@@ -20,7 +29,7 @@ static class BestSumMethods
             if (remainderCombination is not null)
             {
                 var combination = new List<int>(remainderCombination);
-                combination.Add(remainder);
+                combination.Add(num);
                 if (shortestCombination is null || combination.Count < shortestCombination.Count)
                 {
                     shortestCombination = combination;
