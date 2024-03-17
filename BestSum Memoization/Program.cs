62774776f9301e94aa41799d3fbcb381b796bd92
Divan van Zyl <divanvanzyl1234@gmail.com>
2024-03-17 16:19:40 +0200

    Memoized BestSum

--- BestSum Memoization/Program.cs
+++ BestSum Memoization/Program.cs
@@ -1,10 +1,10 @@
 ﻿using Shared;
 using static BestSumMethods;
 
-BestSum(7, [5, 3, 4, 7])!.WriteCollection();
-BestSum(8, [2, 3, 5])!.WriteCollection();
-BestSum(8, [1, 4, 5])!.WriteCollection();
-BestSum(100, [1, 2, 5, 25])!.WriteCollection();
+BestSumMemo(7, [5, 3, 4, 7])!.WriteCollection();
+BestSumMemo(8, [2, 3, 5])!.WriteCollection();
+BestSumMemo(8, [1, 4, 5])!.WriteCollection();
+BestSumMemo(100, [1, 2, 5, 25])!.WriteCollection();
 
 static class BestSumMethods
 {
@@ -26,6 +26,47 @@ static class BestSumMethods
         {
             int remainder = targetSum - num;
             var remainderCombination = BestSum(remainder, numbers);
+            if (remainderCombination is not null)
+            {
+                var combination = new List<int>(remainderCombination);
+                combination.Add(num);
+                if (shortestCombination is null || combination.Count < shortestCombination.Count)
+                {
+                    shortestCombination = combination;
+                }
+            }
+        }
+
+        return shortestCombination;
+    }
+
+    //Memoized tree 
+    //time O(m^2*n )
+    //space O(m^2)
+    internal static List<int>? BestSumMemo(int targetSum, List<int> numbers, Dictionary<int, List<int>?>? memo = null)
+    {
+        if (targetSum == 0) return [];
+        if (targetSum < 0) return null;
+        if (memo is not null && memo.ContainsKey(targetSum))
+        {
+            return memo[targetSum];
+        }
+
+        //Variable to track length of combinations
+        List<int>? shortestCombination = null;
+
+        foreach (var num in numbers)
+        {
+            int remainder = targetSum - num;
+            var remainderCombination = BestSumMemo(remainder, numbers, memo);
+            if(memo is null)
+            {
+                memo = new Dictionary<int, List<int>?> { { remainder, remainderCombination } };
+            }
+            else if(!memo.ContainsKey(remainder))
+            {
+                memo.Add(remainder,remainderCombination);
+            }
             if (remainderCombination is not null)
             {
                 var combination = new List<int>(remainderCombination);
