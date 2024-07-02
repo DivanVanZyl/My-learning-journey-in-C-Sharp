81d88e090c52232e5f9e9e4989e4cc6cb86e2e24
Divan van Zyl <divanvanzyl1234@gmail.com>
2024-07-02 20:50:41 +0200

    Dynamic Programming - Tabulation - Fibonacci

---
+++ Fib Tabulation/Program.cs
@@ -0,0 +1,31 @@
+﻿using Shared;
+using System.Numerics;
+using static FibMethods;
+
+Console.WriteLine(FibTabulation(6));
+Console.WriteLine(FibTabulation(7));
+Console.WriteLine(FibTabulation(8));
+Console.WriteLine(FibTabulation(50));
+
+
+FibTabulation(6);
+
+static class FibMethods
+{
+    //Brute force solution
+    internal static BigInteger FibTabulation(int n)
+    {
+        var table = new List<int>();   // { n + 1 };
+        table.AddRange(Enumerable.Repeat(0, n + 1));    //Fill with zeros
+        table[1] = 1; //Seed index 1 with value of 1.
+
+        for (int i = 0; i < n; i++)
+        {
+            if(i+1 <= n) table[i + 1] += table[i];
+            if (i + 2 <= n) table[i + 2] += table[i];
+        }
+        return table[n];
+    }
+
+
+}
